d5b7bb8f53493bd9aee8f7bfafbbb4ce9e3c67f2
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-27 19:52:49 +0000

    message

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,15 +6,15 @@ import { Analytics } from "@vercel/analytics/react";
 const font = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bird | Design & Development Studio",
-  description: "Grow your business with Bird, a design & development studio.",
+  title: "Globstand technologies | Design & Development Studio",
+  description: "Grow your business with globstand technologies , a design & development studio.",
   openGraph: {
     images: [
-      {
+ 
         url: 'https://www.birdagency.io/logo/logo.webp',
         width: 1200,
         height: 630,
-        alt: 'Bird Logo',
+        alt: 'globstand',
       },
     ],
   },
