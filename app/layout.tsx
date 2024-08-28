92c8e8ec357bd38b9214482833d17ea3d1066cd5
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-28 18:13:56 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,20 @@ import { Analytics } from "@vercel/analytics/react";
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
-
-
-
+export const metadata: Metadata = {
+  title: "Bird | Design & Development Studio",
+  description: "Grow your business with Bird, a design & development studio.",
+  openGraph: {
+    images: [
+      {
+        url: 'https://www.birdagency.io/logo/logo.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Bird Logo',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -17,8 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+
         {children}
         <Analytics />
+
       </body>
     </html>
   );
