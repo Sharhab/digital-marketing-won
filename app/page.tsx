ec6717b43beb8fbd1705affcb8a178125418a0dd
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-28 23:14:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -130,9 +130,9 @@ export default function Home() {
   hover:bg-[#abcbff]
   rounded-[6px]
   border-2
-  border-orange
+  border-[#f59e0b]
   text-white
-  bg-orange
+  bg-[#f59e0b]
   transition
   duration-200
      hover:shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] "
@@ -174,9 +174,9 @@ export default function Home() {
       hover:bg-[#abcbff] 
       rounded-[6px]
       border-2 
-      border-black 
+      border-[#f59e0b]
       dark:border-white 
-       bg-[#121212] 
+       bg-[#f59e0b]
        text-white 
        transition 
        duration-200 
