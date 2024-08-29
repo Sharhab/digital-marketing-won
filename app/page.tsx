4568aa35c28a262223d0b39b75b3da964d90c5e2
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-29 06:37:05 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -299,7 +299,7 @@ export default function Home() {
                   className="object-contain bg-gray-100 p-4 w-full h-40 rounded-md"
                   alt="image"
                 />
-                <h1 className="text-xl font-medium color-[#d97706]">{service.title}</h1>
+                <h1 className="text-xl font-medium text-[#d97706]">{service.title}</h1>
                 <p className="text-gray-500">{service.description}</p>
               </div>
             ))}
@@ -360,8 +360,8 @@ export default function Home() {
         <main className="md:flex items-center justify-center space-y-6 md:space-y-0 md:gap-x-20 xl:w-4/5 2xl:w-[68%] mx-auto px-6 md:px-0">
           <Image
             src={"/logo/logo.webp"}
-            width={10000}
-            height={10000}
+            width={300}
+            height={300}
             className=" md:w-1/3 rounded-md"
             alt="image"
           />
