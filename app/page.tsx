2bc6b21667ccf57d799e69abcaf4b061c7671b1a
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-29 15:31:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ export default function Home() {
             minutes
           </p>
 
-       <div className= "py-5px pb-5px">
+       <div className= "py-5px pb-8px bg-[#333]">
        <MarketingSection />
        </div>
     
