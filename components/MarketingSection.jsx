bbd5a00a062279478697c7757d3669d0fea703c5
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-29 15:13:22 +0100

    Update MarketingSection.jsx

--- components/MarketingSection.jsx
+++ components/MarketingSection.jsx
@@ -1,14 +1,14 @@
 // components/MarketingSection.jsx
 export default function MarketingSection() {
   return (
-    <div className="bg-[#333] p-8 text-white">
+    <div className="bg-[#333] p-8 text-black">
       <div className="space-y-8">
         {/* Section 1 */}
         <div>
-          <h2 className="text-yellow-500 text-2xl font-bold flex items-center">
+          <h2 className="text-orange-600 text-2xl font-bold flex items-center">
             <span className="mr-2">▶</span> DO YOU WANT MORE CUSTOMERS?
           </h2>
-          <p className="text-base mt-2">
+          <p className="text-base mt-2 text-black">
             With the support of our community, you will know the best strategies
             the most successful digital marketers are using to make a ton of
             money online.
@@ -17,11 +17,11 @@ export default function MarketingSection() {
 
         {/* Section 2 */}
         <div>
-          <h2 className="text-yellow-500 text-2xl font-bold flex items-center">
+          <h2 className="text-orangell-500 text-2xl font-bold flex items-center">
             <span className="mr-2">▶</span> DO YOU WANT TO KEEP YOUR CLIENTS
             LONGER?
           </h2>
-          <p className="text-base mt-2">
+          <p className="text-base mt-2 text-black">
             With our all-in-one marketing and sales platform, you will be able
             to keep your tools in one place (while saving a fortune) and
             streamline your entire delivery process so you can focus on keeping
@@ -34,7 +34,7 @@ export default function MarketingSection() {
           <h2 className="text-yellow-500 text-2xl font-bold flex items-center">
             <span className="mr-2">▶</span> DO YOU WANT TO SCALE YOUR BUSINESS?
           </h2>
-          <p className="text-base mt-2">
+          <p className="text-base mt-2 text-black">
             Globstand will help you grow your business by connecting you with
             the most successful digital marketers on the planet who will be able
             to help you close more deals or allow you to offer more services.
