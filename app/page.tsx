ae1576d5b5da1af51e7497705c985eb69043eaac
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-29 18:45:13 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -366,38 +366,6 @@ export default function Home() {
         </main>
       </Element>
 
-      <section>
-        <main className="md:flex items-center justify-center space-y-6 md:space-y-0 md:gap-x-20 xl:w-4/5 2xl:w-[68%] mx-auto px-6 md:px-0">
-          <Image
-            src={"/logo/logo.webp"}
-            width={50}
-            height={50}
-            className="w-14 h-14 md:w-1/3 rounded-md"
-            alt="image"
-          />
-          <div className="flex flex-col gap-y-5 md:w-1/2">
-            <h1 className="text-lg md:text-2xl ">
-               &quot;We&apos;ve been working with globstand
-              for over 2 years and they&apos;ve been amazing to work with. 
-              They&apos;ve helped us grow our business and we couldn&apos;t 
-              be happier with the results.&quot;
-          </h1>
-
-            <div className="flex items-center gap-x-1">
-              <IconStarFilled className="text-4xl text-yellow-500" />
-              <IconStarFilled className="text-4xl text-yellow-500" />
-              <IconStarFilled className="text-4xl text-yellow-500" />
-              <IconStarFilled className="text-4xl text-yellow-500" />
-              <IconStarFilled className="text-4xl text-yellow-500" />
-            </div>
-
-            <span className="text-xl font-medium">
-              Jordan, Brisson <br />
-              CEO, Atlas Massage
-            </span>
-          </div>
-        </main>
-      </section>
 
       <Element name="guarentees">
         <ShootingStarsAndStarsBackgroundDemo />
