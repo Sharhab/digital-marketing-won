bd59acdabf6b432f70e42e74060426d42fdbdfb5
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-29 18:41:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -377,11 +377,12 @@ export default function Home() {
           />
           <div className="flex flex-col gap-y-5 md:w-1/2">
             <h1 className="text-lg md:text-2xl ">
-              &quot;We&apos;ve been working with Bird for over 2 years and
-              they&apos;ve been amazing to work with. They&apos;ve helped us
-              grow our business and we couldn&apos;t be happier with the
-              results. &quot;
-            </h1>
+               &quot;We&apos;ve been working with globstand
+              for over 2 years and they&apos;ve been amazing to work with. 
+              They&apos;ve helped us grow our business and we couldn&apos;t 
+              be happier with the results.&quot;
+          </h1>
+
             <div className="flex items-center gap-x-1">
               <IconStarFilled className="text-4xl text-yellow-500" />
               <IconStarFilled className="text-4xl text-yellow-500" />
