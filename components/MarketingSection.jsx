aefcbaace67b4cddd4e3f3832d63e348c4726e10
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-30 06:23:20 +0100

    Update MarketingSection.jsx

--- components/MarketingSection.jsx
+++ components/MarketingSection.jsx
@@ -1,11 +1,11 @@
 // components/MarketingSection.jsx
 export default function MarketingSection() {
   return (
-    <div className="p-10">
-      <div className="space-y-8 bg-black">
+    <div className="p-8 bg-[#333]">
+      <div className="space-y-8">
         {/* Section 1 */}
         <div>
-          <h2 className="text-yellow-500 text-2xl font-bold flex items-center">
+          <h2 className="text-yellow text-2xl font-bold flex items-center">
             <span className="mr-2">▶</span> DO YOU WANT MORE CUSTOMERS?
           </h2>
           <p className="text-base mt-2 text-white">
@@ -17,11 +17,11 @@ export default function MarketingSection() {
 
         {/* Section 2 */}
         <div>
-          <h2 className="text-yellow-500 text-2xl font-bold flex items-center">
+          <h2 className="text-yellow text-2xl font-bold flex items-center">
             <span className="mr-2">▶</span> DO YOU WANT TO KEEP YOUR CLIENTS
             LONGER?
           </h2>
-          <p className="text-base mt-2">
+          <p className="text-base mt-2 text-whit">
             With our all-in-one marketing and sales platform, you will be able
             to keep your tools in one place (while saving a fortune) and
             streamline your entire delivery process so you can focus on keeping
@@ -31,10 +31,10 @@ export default function MarketingSection() {
 
         {/* Section 3 */}
         <div>
-          <h2 className="text-yellow-500 text-2xl font-bold flex items-center">
+          <h2 className="text-yellow  text-2xl font-bold flex items-center">
             <span className="mr-2">▶</span> DO YOU WANT TO SCALE YOUR BUSINESS?
           </h2>
-          <p className="text-base mt-2">
+          <p className="text-base mt-2 text-white">
             Globstand will help you grow your business by connecting you with
             the most successful digital marketers on the planet who will be able
             to help you close more deals or allow you to offer more services.
