bc4d7c9bf62195ae161583450ba85e8e9dddc984
Sharhab <136917727+Sharhab@users.noreply.github.com>
2024-08-30 23:53:32 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,19 @@
 const nextConfig = {};
 
 export default nextConfig;
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; frame-src https://www.youtube.com; child-src https://www.youtube.com; script-src 'self' https://www.youtube.com; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
+};
